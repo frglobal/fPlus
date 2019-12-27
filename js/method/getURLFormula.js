ff34512c31716664d10d08cf7e21e50df8b3179b
sigurd0414 <sigurd@cco.com.tw>
2019-12-27 21:56:37 +0800

    commit on 'master'

--- js/method/getURLFormula.js
+++ js/method/getURLFormula.js
@@ -1,4 +1,4 @@
-function(fileName) {
+function getURLFormula(fileName) {
 
 return "SQL(\"frplus_dev\",\"SELECT CONCAT(file_path,\'&\',default_op) AS rpath FROM app_list WHERE file_name =\'" + fileName + "\'\",1,1)"
 
